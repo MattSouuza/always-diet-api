182e9b0164d436b1d81cae99ddded8f8493c8cbd
MattSouuza <matheus_ss02@hotmail.com>
2023-05-27 17:02:53 -0300

    new/edit: added new routes for user and meals, also added new migration removing unique constraint from field

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
-// import { z } from 'zod'
+import { z } from 'zod'
 import { validateMealSchema } from '../schemas/meals-schema'
 import { randomUUID } from 'crypto'
 
@@ -30,5 +30,41 @@ export const mealsRoutes = async (app: FastifyInstance) => {
     })
 
     res.status(201).send({ meal: returned[0] })
+  })
+
+  app.get('/:id', async (req, res) => {
+    const mealParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const result = mealParamsSchema.safeParse(req.params)
+
+    if (!result.success) {
+      return res.status(400).send({ error: result.error.issues })
+    }
+
+    const { id } = result.data
+
+    const meal = await knex('meals').where('id', id).first()
+
+    return { meal }
+  })
+
+  app.get('/fromUser/:userid', async (req, res) => {
+    const mealParamsSchema = z.object({
+      userid: z.string().uuid(),
+    })
+
+    const result = mealParamsSchema.safeParse(req.params)
+
+    if (!result.success) {
+      return res.status(400).send({ error: result.error.issues })
+    }
+
+    const { userid: userId } = result.data
+
+    const meals = await knex('meals').where('user_id', userId)
+
+    return { meals }
   })
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,35 +5,44 @@ import { knex } from '../database'
 import { validateUserSchema } from '../schemas/user-schema'
 
 export const usersRoutes = async (app: FastifyInstance) => {
-  app.get('/:id', async (request) => {
+  app.get('/:id', async (req, res) => {
     const userParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
-    const { id } = userParamsSchema.parse(request.params)
+    const result = userParamsSchema.safeParse(req.params)
+
+    if (!result.success) {
+      return res.status(400).send({ error: result.error.issues })
+    }
+
+    const { id } = result.data
 
     const user = await knex('users').where('id', id).first()
 
     return { user }
   })
 
-  app.post('/', async (request, reply) => {
-    const result = validateUserSchema(request)
+  app.post('/', async (req, res) => {
+    const result = validateUserSchema(req)
 
     if (!result.success) {
-      return reply.status(400).send({ error: result.error.issues })
+      return res.status(400).send({ error: result.error.issues })
     }
 
     const { name, dietGoal, email, password } = result.data
 
-    const returned = await knex('users').returning('id').insert({
-      id: randomUUID(),
-      name,
-      diet_goal: dietGoal,
-      email,
-      password,
-    })
-
-    return reply.status(201).send({ user: returned[0] })
+    try {
+      const returned = await knex('users').returning('id').insert({
+        id: randomUUID(),
+        name,
+        diet_goal: dietGoal,
+        email,
+        password,
+      })
+      return res.status(201).send({ user: returned[0] })
+    } catch (error) {
+      return res.status(400).send(error)
+    }
   })
 }
